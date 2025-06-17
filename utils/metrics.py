d4fffbf16ab53ee2141840169ad99c374a891a88
zelunli <zelun.li9@gmail.com>
2025-06-17 22:28:28 +1000

    updated metrics

--- utils/metrics.py
+++ utils/metrics.py
@@ -10,19 +10,33 @@ def normalized_to_best_val_metric(y_pred, all_y_true):
     """
     # Best in the sequence pool -> this should be a constant value
     highest_expression_val = np.max(all_y_true)
-    return y_pred / highest_expression_val
+    highest_y_pred = np.max(y_pred)
+    return highest_y_pred / highest_expression_val
 
 
-def top_10_ratio_intersection_metric(y_pred, all_y_true):
+def top_10_ratio_intersection_metric(y_pred_indices, all_y_true):
     """
-    Calculate the ratio of the top 10% of predicted values to the top 10% of true values.
-    """
-    # Get the top 10% of predicted values
-    top_10_pred = np.sort(y_pred)[-10:]
+    Calculate the intersection ratio between top 10 predicted and true indices.
 
-    # top_10_true should always be the same
-    top_10_true = np.sort(all_y_true)[-10:]
-    return np.mean(top_10_pred) / np.mean(top_10_true)
+    Args:
+        y_pred_indices: Array of predicted values
+        all_y_true: Array of true values
+
+    Returns:
+        float: Ratio of overlapping indices between top 10 predicted and true values
+    """
+    # Get indices of top 10% values
+    num_top_precent = 0.3  # Take top 10% values
+    num_top = int(len(all_y_true) * num_top_precent) # rounded down to the nearest integer
+    # top_pred_indices = np.argsort(y_pred_indices)[-num_top:]
+    top_true_indices = np.argsort(all_y_true)[-num_top:]
+
+    # Find intersection
+    intersection = np.intersect1d(y_pred_indices, top_true_indices)
+
+    # Calculate ratio of intersection size to total size
+    intersection_ratio = len(intersection) / num_top
+    return intersection_ratio
 
 def get_best_value_metric(y_pred):
     """
